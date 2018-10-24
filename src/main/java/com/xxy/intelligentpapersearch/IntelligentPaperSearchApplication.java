33740343795e03a80752d9a95d904ff1631facd5
hhh <957400829@qq.com>
2018-10-24 14:47:39 +0800

    Initial commit

---
+++ src/main/java/com/xxy/intelligentpapersearch/IntelligentPaperSearchApplication.java
@@ -0,0 +1,12 @@
+package com.xxy.intelligentpapersearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentPaperSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentPaperSearchApplication.class, args);
+	}
+}
