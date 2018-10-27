d642ce8634411bae52b120f2bcf9f1002af013b4
hhh <957400829@qq.com>
2018-10-27 20:38:15 +0800

    crud

---
+++ src/main/java/com/xxy/intelligentpapersearch/node/Keyword.java
@@ -0,0 +1,17 @@
+package com.xxy.intelligentpapersearch.node;
+
+import lombok.Data;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+/**
+ * Created by xiongxiaoyu
+ * Data:2018/10/27
+ * Time:19:51
+ */
+
+@NodeEntity
+@Data
+public class Keyword extends BaseEntity {
+	private Long id;
+	private String name;
+}
---
+++ src/test/java/com/xxy/intelligentpapersearch/process/ModelProcessTest.java
@@ -0,0 +1,16 @@
+package com.xxy.intelligentpapersearch.process;
+
+/**
+ * Created by xiongxiaoyu
+ * Data:2018/10/27
+ * Time:12:09
+ */
+public class ModelProcessTest {
+
+	public static void main(String[] args) {
+		ModelProcess a=new ModelProcess();
+		System.out.println(a.questionsPattern);
+
+	}
+
+}
